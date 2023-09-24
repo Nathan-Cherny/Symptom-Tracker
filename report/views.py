37edd4072bd5468e46024d58c778f2cb90369e19
ayushgupta0010 <ayushgupta0010@gmail.com>
2023-09-24 03:31:00 -0400

    Update health score

--- report/views.py
+++ report/views.py
@@ -13,17 +13,15 @@ def index(request):
     if request.method == 'POST':
         data = request.POST.dict()
 
-        print(data)
-
         match data['diagnosed']:
             case "I haven't been diagnosed with anything recently":
-                data['diagnosed_with'] = 'nothing'
+                data['diagnosed'] = 'nothing'
             case "COVID 19":
-                data['diagnosed_with'] = 'covid'
+                data['diagnosed'] = 'covid'
             case "Common Cold":
-                data['diagnosed_with'] = 'common cold'
+                data['diagnosed'] = 'common cold'
             case "Stomach Bug":
-                data['diagnosed_with'] = 'stomach bug'
+                data['diagnosed'] = 'stomach bug'
 
         report_data = {
             'user': User.objects.get(email=request.session['user']['userinfo']['email']),
@@ -33,15 +31,25 @@ def index(request):
             'drank': data.get('drink') == 'on',
             'around_sick': data.get('sick') == 'on',
             'symptoms': {
-                ''
+                'cough': data.get('cough') == 'on',
+                'fever': data.get('fever') == 'on',
+                'chills': data.get('chills') == 'on',
+                'smell': data.get('smell') == 'on',
+                'fatigue': data.get('fatigue') == 'on',
+                'vomit': data.get('vomit') == 'on',
+                'nausea': data.get('nausea') == 'on',
+                'stomach': data.get('stomach') == 'on',
+                'diarrhea': data.get('diarrhea') == 'on',
+                'taste': data.get('taste') == 'on',
+                'breathing': data.get('breathing') == 'on',
+                'nose': data.get('nose') == 'on',
+                'throat': data.get('throat') == 'on'
             }
         }
 
-        print(report_data)
-
-        # report = Report.objects.create(**report_data)
-        # if not report.diagnosed_with == 'nothing':
-        #     Place.objects.filter(name=report.lives_at).update(health_score=F('health_score') + 2)
+        report = Report.objects.create(**report_data)
+        if report.diagnosed_with != 'nothing':
+            Place.objects.filter(name=report.lives_at).update(health_score=F('health_score') + 2)
 
     form = ReportForm()
     return render(request, 'report.html', context={
