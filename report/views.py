201148ef56e5ca52602ce8cac2cedafd0e848b82
ayushgupta0010 <ayushgupta0010@gmail.com>
2023-09-24 01:30:32 -0400

    Save Reports

--- report/views.py
+++ report/views.py
@@ -1,18 +1,36 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
 from .forms import ReportForm
+from .models import Report
 
-import json
+User = get_user_model()
 
 
 def index(request):
-    print(request.method)
-    data = request.POST
+    if request.method == 'POST':
+        data = request.POST.dict()
+        del data['csrfmiddlewaretoken']
 
-    print(data.dict())
+        match data['diagnosed']:
+            case "I haven't been diagnosed with anything recently":
+                data['diagnosed'] = 'nothing'
+            case "COVID 19":
+                data['diagnosed'] = 'covid'
+            case "Common Cold":
+                data['diagnosed'] = 'common cold'
+            case "Stomach Bug":
+                data['diagnosed'] = 'stomach bug'
+
+        report = {
+            'user': User.objects.get(email=request.session['user']['userinfo']['email']),
+            'data': data
+        }
+
+        Report.objects.create(**report)
+
     form = ReportForm()
     return render(request, 'report.html', context={
         'form': form,
         "session": request.session.get("user"),
     })
-    
