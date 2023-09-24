b8512529bb40be7df72b987a242058ce00e41ba7
ayushgupta0010 <ayushgupta0010@gmail.com>
2023-09-24 04:26:26 -0400

    Remove unique from user

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    email = models.EmailField(unique=True)
+    email = models.EmailField()
     name = models.CharField(max_length=200)
     photo = models.URLField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
