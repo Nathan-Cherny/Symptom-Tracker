dfb9974c7ce5b9b4558ae6d971aaa3e11436162e
ayushgupta0010 <ayushgupta0010@gmail.com>
2023-09-24 02:57:15 -0400

    Update report views

--- report/views.py
+++ report/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
+from django.db.models import F
 from django.shortcuts import render
 
+from places.models import Place
 from .forms import ReportForm
 from .models import Report
 
@@ -10,24 +12,33 @@ User = get_user_model()
 def index(request):
     if request.method == 'POST':
         data = request.POST.dict()
-        del data['csrfmiddlewaretoken']
+
+        print(data)
 
         match data['diagnosed']:
             case "I haven't been diagnosed with anything recently":
-                data['diagnosed'] = 'nothing'
+                data['diagnosed_with'] = 'nothing'
             case "COVID 19":
-                data['diagnosed'] = 'covid'
+                data['diagnosed_with'] = 'covid'
             case "Common Cold":
-                data['diagnosed'] = 'common cold'
+                data['diagnosed_with'] = 'common cold'
             case "Stomach Bug":
-                data['diagnosed'] = 'stomach bug'
+                data['diagnosed_with'] = 'stomach bug'
 
-        report = {
+        report_data = {
             'user': User.objects.get(email=request.session['user']['userinfo']['email']),
-            'data': data
+            'diagnosed_with': data.get('diagnosed'),
+            'lives_at': data.get('hall'),
+            'smoked': data.get('smoked'),
+            'drank': data.get('drank'),
+            'around_sick': data.get('around_sick')
         }
 
-        Report.objects.create(**report)
+        print(report_data)
+
+        # report = Report.objects.create(**report_data)
+        # if not report.diagnosed_with == 'nothing':
+        #     Place.objects.filter(name=report.lives_at).update(health_score=F('health_score') + 2)
 
     form = ReportForm()
     return render(request, 'report.html', context={
